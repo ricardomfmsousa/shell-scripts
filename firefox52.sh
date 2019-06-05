72f408ec8696f0c99129fcb9833429b12f3ed1a9
Ricardo Sousa <ricardomfmsousa@gmail.com>
2019-06-05 08:49:09 +0100

    Switch url to point to FF52esr
    Improve script

--- firefox52.sh
+++ firefox52.sh
@@ -5,18 +5,20 @@
 # - Make the binary executable;
 # - Executes the binary with the secified arguments.
 #
-DOWNLOAD_LINK="https://ftp.mozilla.org/pub/firefox/releases/52.0/linux-x86_64/fr/firefox-52.0.tar.bz2"
-DOWNLOAD_TARGET_FILE="firefox-52.0.tar.bz2"
+DOWNLOAD_LINK="https://ftp.mozilla.org/pub/firefox/releases/52.0esr/linux-x86_64/fr/firefox-52.0esr.tar.bz2"
+DOWNLOAD_TARGET_FILE="firefox-52.0esr.tar.bz2"
 DOWNLOAD_TARGET_DIR="/tmp"
 DOWNLOAD_TARGET="$DOWNLOAD_TARGET_DIR/$DOWNLOAD_TARGET_FILE"
 # Path to file after extracting it in the $DOWNLOAD_TARGET_DIR
-BIN_PATH="firefox/firefox"
+BIN_DIR="firefox"
+BIN_NAME="firefox"
+BIN_PATH="$DOWNLOAD_TARGET_DIR/$BIN_DIR/$BIN_NAME"
 # Arguments to pass to the binary
-ARGUMENTS="-private -no-remote http://localhost:4200"
+ARGUMENTS='-no-remote http://localhost:4200'
 
 # Only download if the compressed file does not exist
 if [ ! -f "$DOWNLOAD_TARGET" ]; then
-  wget $DOWNLOAD_LINK -O $DOWNLOAD_TARGET 2>&1 | \
+  wget "$DOWNLOAD_LINK" -O "$DOWNLOAD_TARGET" 2>&1 | \
     sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | \
     zenity --progress \
       --width=600 \
@@ -26,8 +28,10 @@ if [ ! -f "$DOWNLOAD_TARGET" ]; then
       --auto-kill
 fi
 
+
 # Extract all files, overwriting previous
-tar xvfj $DOWNLOAD_TARGET -C $DOWNLOAD_TARGET_DIR --overwrite | \
+rm -r "$DOWNLOAD_TARGET_DIR/$BIN_DIR"
+tar xvfj "$DOWNLOAD_TARGET" -C "$DOWNLOAD_TARGET_DIR" | \
   zenity --progress \
     --pulsate \
     --title="Extracting..." \
@@ -36,7 +40,7 @@ tar xvfj $DOWNLOAD_TARGET -C $DOWNLOAD_TARGET_DIR --overwrite | \
     --auto-kill
 
 # Add execute permissions
-chmod +x $DOWNLOAD_TARGET_DIR/$BIN_PATH
+chmod +x $BIN_PATH
 
 # Run the executable
-$DOWNLOAD_TARGET_DIR/$BIN_PATH $ARGUMENTS
+$BIN_PATH $ARGUMENTS
