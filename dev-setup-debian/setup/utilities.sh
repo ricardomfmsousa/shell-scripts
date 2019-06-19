8edd6d7fb9609902bfc2102444190402f262fe9e
Ricardo Sousa <ricardomfmsousa@gmail.com>
2019-06-19 16:07:09 +0100

    Add packages and PPAs

--- dev-setup-debian/setup/utilities.sh
+++ dev-setup-debian/setup/utilities.sh
@@ -3,11 +3,12 @@
 source ../functions/utils.sh && no-root
 
 PACKAGES=(
-  # Package managers
-  snap gdebi
+  # Package managers, PPAs
+  snap snapd gdebi software-properties-common
   # Runtimes, codecs, fonts and plugins that are restricted in some countries 
-  libavcodec-extra libdvd-pkg
-  ubuntu-restricted-extras ubuntu-restricted-addons fonts-powerline
+  ubuntu-restricted-extras ubuntu-restricted-addons
+  # Codecs
+  libavcodec-extra ffmpeg libdvd-pkg
   # Build essentials 
   build-essential libssl-dev libstdc++6
   # System monitor and power management
@@ -16,15 +17,19 @@ PACKAGES=(
   gparted usb-creator-gtk
   # FTP client
   filezilla
+  # Office
+  libreoffice
   # Graphics
   flameshot gimp gimp-data gimp-plugin-registry 
   gimp-data-extras inkscape imagemagick
   # Video
-  kazam cheese vlc
+  kazam cheese vlc browser-plugin-vlc
   # Fonts
-  fonts-powerline
+  fonts-powerline fonts-liberation
   # Download manager / Torrent client
   curl wget transmission
+  # Games
+  steam
   # Archive Extractors
   unace unrar zip unzip p7zip-full p7zip-rar sharutils
   rar uudeview mpack arj cabextract file-roller
@@ -33,6 +38,8 @@ PACKAGES=(
   # Mouse themes
   breeze-cursor-theme oxygen-cursor-theme oxygen-cursor-theme-extra
 )
+sudo add-apt-repository -y ppa:oguzhaninan/stacer && \
+sudo apt update && \
 sudo apt install -y ${PACKAGES[@]} && \
 
 # Install snap packages
