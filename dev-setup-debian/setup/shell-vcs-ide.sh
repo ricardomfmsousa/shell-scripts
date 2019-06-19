ffadf0f9c47df55791b95c5f6a9fdc7d8ade0a4e
Ricardo Sousa <ricardomfmsousa@gmail.com>
2019-06-19 16:06:40 +0100

    Update script running order

--- dev-setup-debian/setup/shell-vcs-ide.sh
+++ dev-setup-debian/setup/shell-vcs-ide.sh
@@ -13,6 +13,17 @@ PACKAGES=(
 )
 sudo apt install -y ${PACKAGES[@]} && \
 
+# Install snap available text editors/IDEs
+sudo snap install notepad-plus-plus && \
+sudo snap install code --classic && \
+sudo snap install intellij-idea-community --classic && \
+sudo snap install android-studio --classic && \
+
+# Install oh-my-zsh: framework for managing zsh configuration
+rm -rf "$HOME/.oh-my-zsh" && \
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended" && \
+export-var "SHELL" "/bin/zsh" && \
+
 # Configure git globally
 gecho "Setting global git configurations" && \
 read -e -p "Enter your git userame: " -i "$(whoami)" NAME && \
@@ -22,15 +33,4 @@ git config --global mergetool.vscode.cmd 'code --wait $MERGED' && \
 git config --global diff.tool vscode && \
 git config --global difftool.vscode.cmd 'code --wait --diff $LOCAL $REMOTE' && \
 git config --global user.name "$NAME" && \
-git config --global user.email "$EMAIL" && \
-
-# Install oh-my-zsh: framework for managing zsh configuration
-rm -rf "$HOME/.oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended" && \
-export-var "SHELL" "/bin/zsh" && \
-
-# Install snap available text editors/IDEs
-sudo snap install notepad-plus-plus && \
-sudo snap install vscode --classic && \
-sudo snap install intellij-idea-community --classic && \
-sudo snap install android-studio --classic
+git config --global user.email "$EMAIL"
