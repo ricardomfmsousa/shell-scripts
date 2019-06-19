25bce5a1e7980ccac77108dab3d9bee2ad2e91e7
Ricardo Sousa <ricardomfmsousa@gmail.com>
2019-06-19 16:05:37 +0100

    Integrate download-install-deb function

--- dev-setup-debian/setup/browsers.sh
+++ dev-setup-debian/setup/browsers.sh
@@ -6,6 +6,5 @@ source ../functions/utils.sh && no-root
 sudo apt install -y chromium-browser firefox && \
 sudo snap install opera && \
 
-# Install Google Chrome 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp && \
-sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+# Install latest Google Chrome
+download-install-deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
