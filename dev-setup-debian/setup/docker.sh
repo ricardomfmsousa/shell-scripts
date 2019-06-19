c63a489829689f6d351487fa545f525886bcb9eb
Ricardo Sousa <ricardomfmsousa@gmail.com>
2019-06-19 16:06:13 +0100

    Install dockstation from github deb release

--- dev-setup-debian/setup/docker.sh
+++ dev-setup-debian/setup/docker.sh
@@ -2,22 +2,27 @@
 # Setup docker, docker-compose, rancher and openshift
 source ../functions/utils.sh && no-root
 
-# Install docker main tools
+# Install main docker tools
 gecho "Installing docker tools"
 PACKAGES=(
   docker # Container platform
   docker-compose # Define and run multi-container Docker applications
-  dockstation # Manage projects based on Docker
 )
 sudo apt install -y ${PACKAGES[@]} && \
 
-# Install openshift CLI
+# Install latest Dockstation: Manage projects based on Docker
+gecho "Installing Dockstation" && \
+LATEST_VERSION=`git-read-latest-release DockStation/dockstation | cut -dv -f2` && \
+download-install-deb \
+"https://github.com/DockStation/dockstation/releases/download/v${LATEST_VERSION}/dockstation_${LATEST_VERSION}_amd64.deb"
+
+# Install latest openshift CLI
 gecho "Installing openshift CLI" && \
 INSTALL_TO="$HOME/.oc" && \
 git-download-latest-release "openshift/origin" "$INSTALL_TO" && \
 add-to-path "$INSTALL_TO" && \
 
-# Install rancher CLI
+# Install latest rancher CLI
 gecho "Installing rancher CLI" && \
 INSTALL_TO="$HOME/.rancher" && \
 git-download-latest-release "rancher/cli" "$INSTALL_TO"  && \
