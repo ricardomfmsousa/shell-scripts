51a459b7060fb5efdb91f107a19b7d140d14b837
Ricardo Sousa <ricardomfmsousa@gmail.com>
2019-06-24 08:14:02 +0100

    Add tilix configuration

--- dev-setup-debian/setup/post-install.sh
+++ dev-setup-debian/setup/post-install.sh
@@ -8,6 +8,10 @@ DOTFILES_DIR="../dotfiles" &&
 cp -r $DOTFILES_DIR/. $HOME &&
 echo "Copied $(ls -Al $DOTFILES_DIR)" &&
 
+# Load tilix config in dconf (dconf dump /com/gexperts/Tilix/ > $HOME/.tilix)
+gecho "Loading Tilix dconf configuration" &&
+dconf load /com/gexperts/Tilix/ < $HOME/.tilix &&
+
 # Clean dependencies
 gecho "Removing uneeded dependencies" &&
 sudo apt autoremove 
