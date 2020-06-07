298ff167535eb6e5dc6e76b124a5e3d7b79f442e
Ricardo Sousa <ricardomfmsousa@gmail.com>
2020-06-07 18:44:38 +0100

    Add script dependencies

--- dev-setup-debian/setup/java.sh
+++ dev-setup-debian/setup/java.sh
@@ -1,16 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Setup Java and Java version switcher
 
-source ../functions/utils.sh && no-root
+source ./functions/utils.sh && no-root
 
-APT=(
+gecho "Installing default JRE/JDK and JDK 8"
+# Add the AdoptOpenJDK APT repository for OpenJDK 8
+wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+sudo apt update
+
+PKGS=(
+  apt-transport-https # Transitional package for https support
+  ca-certificates # Common CA certificates
+  wget # Retrieve files from the web
+  dirmngr # GNU privacy guard - network certificate management service
+  gnupg # GNU privacy guard - a free PGP replacement
+  software-properties-common # Software repository manager (common)
   maven # Build automation tool used primarily for Java projects
   default-jre # Default Java Runtime
   default-jdk # Default Java Dev Kit
-) &&
-apt-install ${APT[@]} &&
+  adoptopenjdk-8-hotspot # OpenJDK 8
+)
+apt-install ${PKGS[@]}
 
-# Install jenv to easily switch between installed java versions
-JENV_INSTALL_DIR="$HOME/.jenv" &&
-rm -rf $JENV_INSTALL_DIR &&
-git clone https://github.com/gcuisinier/jenv.git $JENV_INSTALL_DIR
+gecho "Setting JVM 8 as default"
+yes 1 | sudo update-alternatives --config java || true
+
+echo "Default Java version set to:"
+java -version
+
+
