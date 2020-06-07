d4b76622cf529684c6fc593832b71cee0c0d6223
Ricardo Sousa <ricardomfmsousa@gmail.com>
2020-06-07 18:42:04 +0100

    Add brave browser
    Remove snap packages

--- dev-setup-debian/setup/browsers.sh
+++ dev-setup-debian/setup/browsers.sh
@@ -1,18 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Setup web browsers
 
-source ../functions/utils.sh && no-root
+source ./functions/utils.sh && no-root
 
-APT=(
-  chromium-browser # Google's open-source web browser
-  firefox # Mozilla Foundation open-source web browser
-) &&
-apt-install ${APT[@]} &&
+# Install latest Brave
+#   Free and open-source privacy focused web browser by Brave Software,
+#   founded by the creator of Javascript and former CEO of Mozilla Corporation
+sudo apt install -y apt-transport-https curl
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc |
+  sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" |
+  sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update && sudo apt install -y brave-browser
 
-SNAP=(
-  opera # Opera Software web browser
-) &&
-snap-install ${SNAP[@]} &&
+PKGS=(
+  firefox # Free and open-source web browser developed by Mozilla
+  chromium # Open-source version of Google Chrome
+)
+apt-install ${PKGS[@]}
 
 # Install latest Google Chrome
 download-install-deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+
+# Install Opera web browser
+download-install-deb "https://download3.operacdn.com/pub/opera/desktop/68.0.3618.125/linux/opera-stable_68.0.3618.125_amd64.deb"
