e53bd105024b3217c40b5f2c2e35f9ca2e394e29
Ricardo Sousa <ricardomfmsousa@gmail.com>
2020-06-07 18:45:10 +0100

    Improve syntax and update packages

--- dev-setup-debian/setup/javascript.sh
+++ dev-setup-debian/setup/javascript.sh
@@ -1,30 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Setup Node.js through NVM and install npm packages
 
-source ../functions/utils.sh && no-root
-
-# Needed for some npm packages that require compiling code from source
-apt-install build-essential &&
+source ./functions/utils.sh && no-root
 
 # Setup nvm
-rm -rf "$NVM_DIR" ~/.nvm && export NVM_DIR="" &&
-LATEST_RELEASE=`git-read-latest-release "nvm-sh/nvm"` &&
-gecho "Installing nvm $LATEST_RELEASE" &&
-sh -c "$(curl -sL https://raw.githubusercontent.com/creationix/nvm/$LATEST_RELEASE/install.sh)" &&
-source ~/.profile &&
+rm -rf `printenv NVM_DIR` ~/.nvm && export NVM_DIR=""
+LATEST_RELEASE=`git-read-latest-release "nvm-sh/nvm"`
+gecho "Installing nvm $LATEST_RELEASE"
+sh -c "$(curl -sL https://raw.githubusercontent.com/creationix/nvm/$LATEST_RELEASE/install.sh)"
+source ~/.profile
 
 # Install latest Node.js LTS
-nvm install --lts &&
+nvm install --lts
 
 # Install mandatory node packages
-gecho "Installing node packages" &&
-NPM=(
-  npx # Executes npm package binaries
+gecho "Installing node packages"
+PKGS=(
   yarn # Fast, reliable, and secure dependency management.
   @angular/cli # A command line interface for Angular
-  @vue/cli # Standard Tooling for Vue.js Development
   create-react-app # Create React apps with no build configuration
   typescript # Superset of JavaScript that compiles to clean JavaScript output
-) &&
-npm i -g ${NPM[@]}
+)
+# Install globally, silently, quietly, accept defaults, anwser no all questions
+yes n | npm i -g -s -q -y ${PKGS[@]} || true
 
