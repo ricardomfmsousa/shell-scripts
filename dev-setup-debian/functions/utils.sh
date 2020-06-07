5d05f40e4769bea1b943dd827229c15b74b583eb
Ricardo Sousa <ricardomfmsousa@gmail.com>
2020-06-07 18:41:06 +0100

    Add proper fail-first strategy
    Make the setup the less interactive possible
    Improve syntax
    Add apt-wait to prevent fails due to longer running operations
    Add flatpack install script
    Add app image install script
    Add lock file

--- dev-setup-debian/functions/utils.sh
+++ dev-setup-debian/functions/utils.sh
@@ -1,4 +1,16 @@
+#!/usr/bin/env bash
 # Utility functions to be used in `setup.sh`
+
+# Exit immediately if a command exits with a non-zero status.
+# Treat unset variables as an error when substituting.
+# Set the return value of a pipeline to the status of last command to exit with a non-zero status.
+set -euo pipefail
+# Print the errored script error code, name and line
+trap 'rc=$?; recho "Error code $rc in $0 at line $LINENO"; exit $rc' ERR
+# Disable some (text) dialogs and use default values where possible
+DEBIAN_FRONTEND=noninteractive
+
+APP_IMAGE_FOLDER="$HOME/.local/bin"
 
 recho() { echo -e "\e[1m\e[31m$1\e[0m"; }
 gecho() { echo -e "\e[1m\e[32m$1\e[0m"; }
@@ -6,6 +18,7 @@ becho() { echo -e "\e[1m\e[34m$1\e[0m"; }
 
 no-root() {
   [ "$EUID" -eq 0 ] && echo "Please don't run $0 as root..." && exit 1
+  return 0
 }
 
 # $1: repo name
@@ -34,7 +47,8 @@ git-download-latest-release() {
 add-to-path() {
   # Only add if not in $PATH already
   [[ ":$PATH:" == *":$1:"* ]] && return 0
-  echo "export PATH=\"$1:$PATH\"" >> $HOME/.profile &&
+  echo 'if [ -d "'$1'" ]; then PATH="'$1':$PATH"; fi' >> $HOME/.profile &&
+  sleep 1 &&
   source $HOME/.profile
 }
 
@@ -81,24 +95,23 @@ apt-wait() {
 # $@: Packages to install
 apt-install() {
   packages=("$@")
-  sudo apt install -y "${packages[@]}"
+  apt-wait &&
+  sudo apt install -y "${packages[@]}" &&
+  apt-wait
 }
 
 # $1: PPA name
 # $2: package name
 apt-install-ppa() {
   # Install software-properties-common to get add-apt-repository
-  sudo apt install -y software-properties-common &&
   sudo add-apt-repository -y -u ppa:$1 && 
   sudo apt install -y $2
 }
 
 # $@: Snap packages to install
 snap-install() {
-  # Ensure that the snap binaries are installed
-  sudo apt install -y snap snapd &&
   # Install one by one to prevent fails when in bulk and a snap exists
-  packages=("$@") &&
+  packages=("$@")
   for snap in "${packages[@]}"; do
     # Dynamicaly read and set confinement
     confinement=`snap info --verbose $snap |
@@ -107,9 +120,40 @@ snap-install() {
   done
 }
 
+# $@: Flatpack packages to install
+flatpack-install() {
+  packages=("$@")
+  flatpak install -y flathub "${packages[@]}"
+}
+
+# $1: AppImage URL to install
+app-image-install() {
+  mkdir -p $APP_IMAGE_FOLDER && 
+  cd $_ &&
+  wget $1 &&
+  filename=$(basename $1) &&
+  chmod a+x "$APP_IMAGE_FOLDER/$filename" &&
+  cd -
+}
+
 # $1: download url 
 download-install-deb() {
   DEB_NAME=`mktemp` &&
-  sudo apt install -y gdebi &&
   wget -O "$DEB_NAME" "$1" && sudo gdebi -n "$DEB_NAME" && rm -f "$DEB_NAME"
 }
+
+# Install/configure packages to be used further in the install
+# process, making sure all script dependecies are met
+LOCK_FILE=/tmp/utils.lock
+if [[ ! -f "$LOCK_FILE" ]]; then
+  becho "Installing utils dependencies"
+  sudo apt update && sudo apt upgrade -y && 
+  apt-install \
+    wget python3-pip git dirmngr build-essential gdebi snap snapd \
+    gdebi software-properties-common python3-software-properties
+  # Do not run this init in the current session anymore
+  touch "$LOCK_FILE" &&
+  becho "Utils dependencies installed."
+  sleep 1
+fi
+
