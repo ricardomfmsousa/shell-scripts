c70d4f3975ce73e2cbb8ed7b2857bddbd556a43c
Ricardo Sousa <ricardomfmsousa@gmail.com>
2020-06-07 18:47:05 +0100

    Add notify watchers
    Fix paths

--- dev-setup-debian/setup/post-install.sh
+++ dev-setup-debian/setup/post-install.sh
@@ -1,18 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Copy dotfiles and perform some cleanup
 
-source ../functions/utils.sh && no-root
+source ./functions/utils.sh && no-root
 
 # Copy all configuration files under the dotfiles folder
 gecho "Copying dotfiles"
-DOTFILES_DIR="../dotfiles" &&
-cp -r $DOTFILES_DIR/. $HOME &&
-echo "Copied $(ls -Al $DOTFILES_DIR)" &&
+DOTFILES_DIR="dotfiles"
+cp -r $DOTFILES_DIR/. $HOME
+echo "Copied $(ls -Al $DOTFILES_DIR)"
 
-# Load tilix config in dconf (dconf dump /com/gexperts/Tilix/ > $HOME/.tilix)
-gecho "Loading Tilix dconf configuration" &&
-dconf load /com/gexperts/Tilix/ < $HOME/.tilix &&
+# Increase the amount of inotify watchers
+# https://github.com/angular/angular-cli/issues/8168
+# https://github.com/guard/listen/wiki/Increasing-the-amount-of-inotify-watchers
+gecho "Updating inotify watchers"
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
 
 # Clean dependencies
-gecho "Removing uneeded dependencies" &&
-sudo apt autoremove 
+gecho "Removing unneeded dependencies"
+sudo apt autoremove
+
+# TODO: create swap file, enable it and configure swapiness
