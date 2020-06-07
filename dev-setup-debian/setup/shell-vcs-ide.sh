c4c894a4bf2e1ae8c9e35359a6a7dbec8dbfaa0e
Ricardo Sousa <ricardomfmsousa@gmail.com>
2020-06-07 18:48:45 +0100

    Remove snap packages

--- dev-setup-debian/setup/shell-vcs-ide.sh
+++ dev-setup-debian/setup/shell-vcs-ide.sh
@@ -1,39 +1,40 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Setup all related shell, terminal, VCS and editor software
 
-source ../functions/utils.sh && no-root
+source ./functions/utils.sh && no-root
 
-APT=(
+PKGS=(
   zsh # Extended Bourne shell with a large number of improvements
   tmux # Terminal multiplexer
-  tilix # Advanced GTK3 tiling terminal emulator 
+  tilix # Advanced and clean GTK3 tiling terminal emulator 
   vim # Highly configurable text editor
   git # Fast, scalable, distributed revision control system
   git-gui # A Tcl/Tk based graphical user interface to Git
   meld # Visual diff and merge tool
-) &&
-apt-install ${APT[@]} &&
-
-SNAP=(
   code # TypeScript IDE from Microsoft
-  android-studio # Official IDE for Google's Android operating system
-  notepad-plus-plus # Good old M$ Windows notepad++ packed in a snap
-  intellij-idea-community # Java IDE from JetBrains
-) &&
-snap-install ${SNAP[@]} && 
+)
+apt-install ${PKGS[@]}
+
+PKGS=(
+  com.github.lainsce.notejot # A stupidly-simple sticky notes application
+  com.jetbrains.IntelliJ-IDEA-Community # Java IDE from JetBrains
+  com.jetbrains.IntelliJ-IDEA-Ultimate # Java IDE from JetBrains
+)
+flatpack-install ${PKGS[@]}
+
+# Change the default shell to zsh
+sudo chsh -s "/bin/zsh" "$USER"
 
 # Install oh-my-zsh: framework for managing zsh configuration
-rm -rf "$HOME/.oh-my-zsh" &&
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended" &&
-export-var "SHELL" "/bin/zsh" &&
+rm -rf "$HOME/.oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
+
+# Generate a new SSH key pair for fast, easy and secure git authentication
+yes y | ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa || true
 
 # Configure git globally
-gecho "Setting global git configurations" &&
-read -e -p "Enter your git userame: " -i "$(whoami)" NAME &&
-read -e -p "Enter your git email: " EMAIL &&
-git config --global merge.tool vscode &&
-git config --global mergetool.vscode.cmd 'code --wait $MERGED' &&
-git config --global diff.tool vscode &&
-git config --global difftool.vscode.cmd 'code --wait --diff $LOCAL $REMOTE' &&
-git config --global user.name "$NAME" &&
+gecho "Setting global git configurations"
+read -e -p "Enter your git userame: " -i "$(whoami)" NAME
+read -e -p "Enter your git email: " EMAIL
+git config --global user.name "$NAME"
 git config --global user.email "$EMAIL"
