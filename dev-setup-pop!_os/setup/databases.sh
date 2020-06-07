d7ff139f2cc30e55b0ac1f2679e23b651d184101
Ricardo Sousa <ricardomfmsousa@gmail.com>
2020-06-07 20:07:56 +0100

    Fix mongodb package repository

--- dev-setup-pop!_os/setup/databases.sh
+++ dev-setup-pop!_os/setup/databases.sh
@@ -7,11 +7,11 @@ source ./functions/utils.sh && no-root
 download-install-deb 'https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb'
 
 # Install MongoDB Compass - Official GUI for MongoDB
-download-install-deb 'https://downloads.mongodb.com/compass/mongodb-compass_1.20.5_amd64.deb'
+download-install-deb 'https://downloads.mongodb.com/compass/mongodb-compass_1.21.2_amd64.deb'
 
 # Install mongodb from the official repo
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - 
-echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.2 main" | \
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | 
   sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt update
 apt-install mongodb-org
