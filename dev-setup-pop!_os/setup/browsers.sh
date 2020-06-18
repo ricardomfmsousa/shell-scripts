4d35c2a56b0b44633a0bd4d6005a90b103d3985a
Ricardo Sousa <ricardomfmsousa@gmail.com>
2020-06-18 08:44:00 +0100

    Update repository info after adding

--- dev-setup-pop!_os/setup/browsers.sh
+++ dev-setup-pop!_os/setup/browsers.sh
@@ -11,11 +11,12 @@ curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc |
   sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" |
   sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update && sudo apt install -y brave-browser
 
 # Add Opera web browser repository
 sudo add-apt-repository 'deb https://deb.opera.com/opera-stable/ stable non-free'
 wget -qO - https://deb.opera.com/archive.key | sudo apt-key add -
+
+sudo apt update
 
 PKGS=(
   firefox # Free and open-source web browser developed by Mozilla
