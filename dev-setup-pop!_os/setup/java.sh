aa3297ba25434f35d537a4d38c57640bf54acad4
Ricardo Sousa <ricardomfmsousa@gmail.com>
2020-06-18 08:05:50 +0100

    Fix jdk8 install (invalid release file for ppa)

--- dev-setup-pop!_os/setup/java.sh
+++ dev-setup-pop!_os/setup/java.sh
@@ -3,13 +3,6 @@
 
 source ./functions/utils.sh && no-root
 
-gecho "Installing default JRE/JDK and JDK 8"
-# Install OpenJDK 8 from AdoptOpenJDK APT repository
-wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
-apt-install-ppa \
-  https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ \
-  adoptopenjdk-8-hotspot
-
 PKGS=(
   apt-transport-https # Transitional package for https support
   ca-certificates # Common CA certificates
@@ -18,13 +11,15 @@ PKGS=(
   gnupg # GNU privacy guard - a free PGP replacement
   software-properties-common # Software repository manager (common)
   maven # Build automation tool used primarily for Java projects
+  openjdk-8-jre # Java 8 Runtime
+  openjdk-8-jdk # Java 8 Dev Kit
   default-jre # Default Java Runtime
   default-jdk # Default Java Dev Kit
 )
 apt-install ${PKGS[@]}
 
 gecho "Setting JVM 8 as default"
-yes 1 | sudo update-alternatives --config java || true
+yes 2 | sudo update-alternatives --config java || true
 
 echo "Default Java version set to:"
 java -version
