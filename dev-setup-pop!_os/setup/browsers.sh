118776cda3aec76261c65e79a2177f8b6fde6196
Ricardo Sousa <ricardomfmsousa@gmail.com>
2020-06-18 08:41:38 +0100

    Update opera install method

--- dev-setup-pop!_os/setup/browsers.sh
+++ dev-setup-pop!_os/setup/browsers.sh
@@ -3,7 +3,7 @@
 
 source ./functions/utils.sh && no-root
 
-# Install latest Brave
+# Add Brave web browser repository
 #   Free and open-source privacy focused web browser by Brave Software,
 #   founded by the creator of Javascript and former CEO of Mozilla Corporation
 sudo apt install -y apt-transport-https curl
@@ -13,14 +13,20 @@ echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable ma
   sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update && sudo apt install -y brave-browser
 
+# Add Opera web browser repository
+sudo add-apt-repository 'deb https://deb.opera.com/opera-stable/ stable non-free'
+wget -qO - https://deb.opera.com/archive.key | sudo apt-key add -
+
 PKGS=(
   firefox # Free and open-source web browser developed by Mozilla
   chromium # Open-source version of Google Chrome
+  brave-browser # Chromium-based open-source privacy focused web browser
+  opera-stable # Chromium-based browser using the Blink layout engine
 )
 apt-install ${PKGS[@]}
 
 # Install latest Google Chrome
 download-install-deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 
-# Install Opera web browser
-download-install-deb "https://download3.operacdn.com/pub/opera/desktop/68.0.3618.125/linux/opera-stable_68.0.3618.125_amd64.deb"
+
+
