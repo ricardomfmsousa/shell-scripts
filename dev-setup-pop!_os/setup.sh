b749f3bb6aa5d4ac7961b66e61711892ea724306
Ricardo Sousa <ricardomfmsousa@gmail.com>
2020-08-27 19:51:26 +0100

    Add script description and bechmark

--- dev-setup-pop!_os/setup.sh
+++ dev-setup-pop!_os/setup.sh
@@ -2,7 +2,28 @@
 # Sets up a web development environment by running all scripts located under the
 # `setup` directory *(all scripts can also be run individually)*
 
-source ./functions/utils.sh && no-root
+source ./functions/base-utils.sh && no-root
+
+# Display informations and perform all questions, setting further needed
+# variables in advance, to run all the setup script non-interactively
+clear
+becho "************************************************"
+gecho " This script will install and configure common"
+gecho " core software tools for web development."
+echo
+gecho " It has been tested in Pop!_OS 20.04 but it"
+gecho " should also work in Ubuntu and derivates."
+becho "************************************************"
+read -e -p "Enter your git username: " -i "$(whoami)" GIT_USER_NAME
+read -e -p "Enter your git email: " GIT_USER_EMAIL
+export GIT_USER_NAME="$GIT_USER_NAME"
+export GIT_USER_EMAIL="$GIT_USER_EMAIL"
+becho "Press [ENTER] to start the setup, [Ctrl+C] to cancel."; read -p ""
+
+# Count how much time the setup script took to finish
+SECONDS=0
+
+source ./functions/utils.sh
 
 # Declare which setup files to run, under the setup directory
 SETUP_DIR="setup"
@@ -23,11 +44,13 @@ SETUP_FILES=(
 for setup in ${SETUP_FILES[@]}; do 
   becho "Running $setup"
   # Warn and exit immediately if child script errors 
-  "$SETUP_DIR"/./"$setup"
+  "$SETUP_DIR/$setup"
   [ $? -ne 0 ] && recho "Error in $setup" && exit $?
 done
 
+gecho "Done with success!"
+gecho "Setup finished in $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds."
+
 # Reboot
-gecho "Setup success!"
-becho "Press enter to REBOOT, Ctrl+C to cancel."; read -p ""
+becho "Press [ENTER] to reboot, [Ctrl+C] to cancel."; read -p ""
 sudo reboot
